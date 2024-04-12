dfd2b80d660b131da14847de8e289b0ec27d8b30
Hoda Sedighi <sedighi.iut@gmail.com>
2024-04-12 11:41:21 -0400

    implementing the longest common prefix solution

---
+++ MyMethod.cs
@@ -0,0 +1,23 @@
+using System;
+
+public class Solution {
+    public string LongestCommonPrefix(string[] strs) {
+        if (strs == null || strs.Length == 0) return "";
+
+        int minLength = int.MaxValue;
+        foreach (string str in strs) {
+            minLength = Math.Min(minLength, str.Length);
+        }
+
+        for (int i = 0; i < minLength; i++) {
+            char currentChar = strs[0][i];
+            for (int j = 1; j < strs.Length; j++) {
+                if (strs[j][i] != currentChar) {
+                    return strs[0].Substring(0, i);
+                }
+            }
+        }
+
+        return strs[0].Substring(0, minLength);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+           //call solution class from mymethod.cs
+            Solution sol = new Solution();
+            string[] strs = new string[] { "flower", "flow", "flight" };
+            Console.WriteLine(sol.LongestCommonPrefix(strs));
+    }
+}
